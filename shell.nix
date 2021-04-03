08ccc3385a8fc086fb56df3dff404b94dc017d6c
Mark Gerarts <mark.gerarts@gmail.com>
2021-04-03 10:51:23 +0200

    [WIP] Set up frontend/backend dev environment

--- shell.nix
+++ shell.nix
@@ -1,17 +1,37 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}}:
 
 with pkgs;
 
 let
 
-  yesod-devel = writeScriptBin "yesod-devel" ''
-    ${haskellPackages.yesod-bin}/bin/yesod devel
-  '';
+  pursPin = import (builtins.fetchGit {
+      name = "purescript-unstable-pin";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixos-unstable";
+      rev = "04a2b269d8921505a2969fc9ec25c1f517f2b307";
+  }) {};
 
   local-hoogle = writeScriptBin "local-hoogle" ''
     ${stack}/bin/stack test --fast --haddock-deps
     ${haskellPackages.hoogle}/bin/hoogle generate --local=./
     ${haskellPackages.hoogle}/bin/hoogle server --local --port=8080
+  '';
+
+  serve-backend = writeScriptBin "serve-backend" ''
+    ${haskellPackages.yesod-bin}/bin/yesod devel
+  '';
+
+  serve-ui = writeScriptBin "serve-ui" ''
+    npm run build --prefix=frontend && npm run serve --prefix=frontend
+  '';
+
+  serve-both = writeScriptBin "serve-both" ''
+    sh -c 'serve-ui & serve-backend'
+  '';
+
+  # @todo: stack prod build
+  build-prod = writeScriptBin "build-prod" ''
+    npm run build-prod --prefix=frontend
   '';
 
 in mkShell {
@@ -23,10 +43,20 @@ in mkShell {
 
   buildInputs = [
     # Scripts
-    yesod-devel
     local-hoogle
+    serve-backend
+    serve-ui
+    serve-both
+    build-prod
 
     stack
     cabal-install
+
+    ## Frontend
+    pursPin.purescript
+    spago
+    nodejs
+    nodePackages.parcel-bundler
+    nodePackages.purty
   ];
 }
--- src/Application.hs
+++ src/Application.hs
@@ -5,7 +5,9 @@
 module Application (application) where
 
 import Data.Aeson (KeyValue ((.=)), ToJSON (toJSON), object)
-import Servant (Application, Get, JSON, Server, serve, (:>))
+import Network.Wai.Application.Static (defaultWebAppSettings, ssIndices)
+import Servant
+import WaiAppStatic.Types (unsafeToPiece)
 
 data Task a where
   Update :: Text -> Task Text
@@ -18,14 +20,30 @@ instance ToJSON (Task a) where
 
 type TaskAPI = "current-task" :> Get '[JSON] (Task Text)
 
+type StaticAPI = Raw
+
+type API = TaskAPI :<|> StaticAPI
+
 currentTask :: Task Text
 currentTask = Update "Edit me!"
 
-server :: Server TaskAPI
-server = return currentTask
+server :: Server API
+server = taskServer :<|> staticServer
+  where
+    taskServer :: Server TaskAPI
+    taskServer = return currentTask
 
-userAPI :: Proxy TaskAPI
-userAPI = Proxy
+    staticServer :: Server StaticAPI
+    staticServer =
+      -- serveDirectoryWebAbb does not automatically use index.html when
+      -- visiting /, so we use the default webApp settings, but override the
+      -- fallback.
+      let defaultSettings = defaultWebAppSettings "frontend/prod"
+          indexFallback = map unsafeToPiece ["index.html"]
+       in serveDirectoryWith <| defaultSettings {ssIndices = indexFallback}
+
+apiProxy :: Proxy API
+apiProxy = Proxy
 
 application :: Application
-application = serve userAPI server
+application = serve apiProxy server
